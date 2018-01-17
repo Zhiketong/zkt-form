d4320f9ec1b769e892f58e700af734dd4167afa1
MillerRen <ren12345@126.com>
2018-01-17 09:21:57 +0800

    utils

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -33,7 +33,7 @@
 </template>
 <script>
   import {validationMixin} from 'vuelidate'
-  import * as validators from 'vuelidate/lib/validators'
+  import * as utils from './utils.js'
   import FormGroup from './Group.vue'
   import FormCol from './Col.vue'
   import FormLabel from './Label.vue'
@@ -45,40 +45,8 @@
   import FormSelect from './Select.vue'
   import FormStatic from './Static.vue'
 
-  function typeOf (obj) {
-    var class2type = {} ;
-    "Boolean Number String Function Array Date RegExp Object Error"
-    .split(" ")
-    .forEach(function(e,i){
-        class2type[ "[object " + e + "]" ] = e.toLowerCase()
-    })
-    if ( obj == null ){
-        return String( obj )
-    }
-    return typeof obj === "object" || typeof obj === "function" ?
-        class2type[ class2type.toString.call(obj) ] || "object" :
-        typeof obj
-  }
-
-  // 转换校验配置规则，把配置的值转换成函数调用
-  function transformValidation (validation) {
-    for(let name in validation) {
-      let validator = validation[name]
-      for(let k in validator) {
-        if (typeOf(validator[k]) === 'object') {
-          validation[name] = transformValidation(validation[name])
-          break
-        }
-        if (!validators[k]) continue
-        validator[k] = validator[k]===true ? validators[k] : validators[k](validator[k])
-      }
-    }
-    return validation
-  }
-
   export default {
     name: 'Form',
-    validators,
     mixins: [validationMixin],
     components: {
       FormGroup,
@@ -116,7 +84,7 @@
       }
     },
     validations () {
-      var validation = transformValidation(this.validation)
+      var validation = utils.transformValidation(this.validation)
       return {
         value: this.validation
       }
