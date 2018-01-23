e3ce3f50e298c991a5d028327e9068d9cbeeeb6e
MillerRen <ren12345@126.com>
2018-01-23 19:41:57 +0800

    expression

--- src/components/Static.vue
+++ src/components/Static.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="form-control-static" v-html="value[name]"></div>
+  <div class="form-control-static" v-html="t(value[name])"></div>
 </template>
 <script>
   import Base from './Base.vue'
 
   export default {
     name: 'FormStatic',
-    extends: Base
+    extends: Base,
+    methods: {
+      t () {
+        return this.computedValue || this.value[this.name]
+      }
+    }
   }
 </script>
