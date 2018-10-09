eb7d6a1c8057b3ccaebd77ba59dd3cfe6cc0c81b
MillerRen <ren12345@126.com>
2018-10-09 09:30:52 +0800

    ..

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,22 +1,18 @@
 <template>
-  <div>
-    <slot name='label'></slot>
-    <select
+  <select
     v-model="val"
     v-bind="$props"
     class="form-control"
     @change="$emit('change', $event.target.value)">
-      <option value="">请选择</option>
-      <option
-      v-for="option in options"
-      :value="option.value"
-      :key="option.value"
-      >
-        {{option.label}}
-      </option>
-    </select>
-    <slot ></slot>
-  </div>
+    <option value="">请选择</option>
+    <option
+    v-for="option in options"
+    :value="option.value"
+    :key="option.value"
+    >
+      {{option.label}}
+    </option>
+  </select>
 </template>
 <script>
   import Base from './Base.vue'
