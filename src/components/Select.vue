6d6a2e3ed0b4a31c9499ff873306956eefc3a478
dongjinglong <dongjinglong@zhiketong.cn>
2020-01-09 11:44:28 +0800

    添加禁用请选择和禁用option

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -3,11 +3,12 @@
     v-model="val"
     v-on="listeners"
     class="form-control">
-    <option value="">请选择</option>
+    <option value="" v-if="showEmpty">请选择</option>
     <option
     v-for="option in options"
     :value="option[optionValueName]"
     :key="option[optionKeyName]"
+    :disabled="option[disabled]"
     >
       {{option[optionKeyName]}}
     </option>
@@ -19,6 +20,10 @@
     name: 'Select',
     extends: Base,
     props: {
+      showEmpty: {
+        type: Boolean,
+        default: true
+      },
       name: {
         type: String,
         default: ''
